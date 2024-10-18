7172e656e8a145bbcaec0179cbde40dd3e7644f9
Caio Martin <162186691+Caio-Martin@users.noreply.github.com>
2024-10-18 20:39:16 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -10,7 +10,7 @@ Escrever(db)
 st.title("MEU 1º WEB APP STREAMLIT")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Hejheheheh! Prof. Massaki")
+st.header("Testando funcionalidades no Streamlit")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Sub Cabeçalho")
@@ -20,3 +20,14 @@ st.write("Como já deve ter percebido, o método st.write() é usado para escrit
 
 values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
