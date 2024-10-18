390d7e6044dc1691b16d3a19e4b1f0b19ee32c2b
Caio Martin <162186691+Caio-Martin@users.noreply.github.com>
2024-10-18 20:45:55 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,29 +1,26 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
+import pandas as pd  # Certifique-se de importar pandas
 from ACTlib01 import *
+
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
 db = Ler_GooglePlanilha(url)
 db.fillna('', inplace=True)
 Escrever(db)
 
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
+# Adicionando título e cabeçalhos
 st.title("MEU 1º WEB APP STREAMLIT")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Testando funcionalidades no Streamlit")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Sub Cabeçalho")
-
-# Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
+# Adicionando um slider
 values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
 
+# Configurando a URL e a coluna de data
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -31,3 +28,14 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
+
+# Carregando os dados
+data_load_state = st.text('Carregando dados...')
+data = load_data(1000)  # Carregando as primeiras 1000 linhas
+data_load_state.text('Dados carregados!')
+
+# Exibindo os dados
+st.subheader('Dados carregados')
+st.write(data)
+
+# Opcional: você pode adicionar gráficos ou análises adicionais com os dados carregados
